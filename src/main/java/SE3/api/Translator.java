049eda063888bc2745cfaea809cce5f9dced8618
CallumHerr <calherring@outlook.com>
2024-03-02 23:27:50 +0000

    Updated to maven, added TTS

---
+++ src/main/java/SE3/api/Translator.java
@@ -0,0 +1,4 @@
+package SE3.api;
+
+public class Translator {
+}
---
+++ src/main/java/SE3/gui/ButtonListener.java
@@ -0,0 +1,12 @@
+package SE3.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
